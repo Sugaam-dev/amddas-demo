79ab7c564ce5d43f2f5254e9fcbc18117fa6069d
Sugaam-dev <sugaam.dev@gmail.com>
2025-08-19 23:20:30 +0530

    Update Why_Us.jsx

--- src/Components/WhyUs/Why_Us.jsx
+++ src/Components/WhyUs/Why_Us.jsx
@@ -16,7 +16,7 @@ const sections = [
   },
   {
     title: "Quality Standard",
-    image: "/images/qualitystandard123.webp",
+    image: "/images/Employeestandard2.png",
     points: [
       "Food is cooked in a very hygienic environment under constant quality controlled processes.",
       "Vegetables are procured twice/week for freshness from authorized Distributor (Qtrly Pesticide residual test done)",
